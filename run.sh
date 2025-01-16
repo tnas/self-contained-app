c45f238835dc3815d1c4fa7330422f1abbb60b18
Thiago Nascimento <nascimenthiago@gmail.com>
2025-01-16 12:15:36 -0300

    Add commands to download, compile, and run

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/11.0.25+9/openlogic-openjdk-11.0.25+9-linux-x64.tar.gz
-tar -xvf openlogic-openjdk-11.0.25+9-linux-x64.tar.gz -C jdk/linux
-mv jdk/linux/openlogic-openjdk-11.0.25+9-linux-x64 openjdk-11
+JDK_NAME="openlogic-openjdk-11.0.25+9-linux-x64"
+JDK_ZIP="${JDK_NAME}.tar.gz"
+JDK_DIR="jdk/linux"
+JDK_HOME="${JDK_DIR}/openjdk-11"
+JDK_BIN="${JDK_HOME}/bin"
 
-JDK_DIR="jdk/openjdk-11/bin"
+if [ ! -d $JDK_HOME ]; then
+	mkdir $JDK_DIR
+	wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/11.0.25+9/$JDK_ZIP
+	tar -xvf $JDK_ZIP -C $JDK_DIR
+	mv $JDK_DIR/$JDK_NAME $JDK_HOME
+	rm -f $JDK_ZIP
+fi
 
-${JDK_DIR}/javac -cp "lib/*" -d out src/java/io/github/tnas/webapp/SimpleWebApp.java
-
-${JDK_DIR}/java -cp "out:lib/*" io.github.tnas.webapp.SimpleWebApp
+${JDK_BIN}/javac -cp "lib/*" -d out src/java/io/github/tnas/webapp/SimpleWebApp.java
+${JDK_BIN}/java -cp "out:lib/*" io.github.tnas.webapp.SimpleWebApp
