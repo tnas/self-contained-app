bd6c69a2c67d02568104f6bfc534b96e13044027
Thiago Nascimento <nascimenthiago@gmail.com>
2025-01-16 08:08:10 -0300

    Add commands to download unzip and rename openjdk for linux

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-javac -cp "lib/*" -d out src/java/io/github/tnas/webapp/SimpleWebApp.java
+wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/11.0.25+9/openlogic-openjdk-11.0.25+9-linux-x64.tar.gz
+tar -xvf openlogic-openjdk-11.0.25+9-linux-x64.tar.gz -C jdk/linux
+mv jdk/linux/openlogic-openjdk-11.0.25+9-linux-x64 openjdk-11
 
-java -cp "out:lib/*" io.github.tnas.webapp.SimpleWebApp
+JDK_DIR="jdk/openjdk-11/bin"
+
+${JDK_DIR}/javac -cp "lib/*" -d out src/java/io/github/tnas/webapp/SimpleWebApp.java
+
+${JDK_DIR}/java -cp "out:lib/*" io.github.tnas.webapp.SimpleWebApp
