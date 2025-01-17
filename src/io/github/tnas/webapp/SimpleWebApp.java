e628c4339b35ae9244979ed7e7746fd44a163f15
Thiago Nascimento <nascimenthiago@gmail.com>
2025-01-17 11:12:04 -0300

    feature: Landing page for engsoftware

--- src/io/github/tnas/webapp/SimpleWebApp.java
+++ src/io/github/tnas/webapp/SimpleWebApp.java
@@ -47,8 +47,8 @@ public class SimpleWebApp {
         tomcat.addServlet(contextPath, servletName, servlet);
         context.addServletMappingDecoded(urlPattern, servletName);
 
-        var webappDirLocation = "webapp"; 
-        tomcat.addWebapp("/app", new File(webappDirLocation).getAbsolutePath());
+        var webappDirLocation = "webapp/engsoftware";
+        tomcat.addWebapp("/engsoftware", new File(webappDirLocation).getAbsolutePath());
         
         tomcat.start();
         tomcat.getServer().await();
